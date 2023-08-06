7a5176f6cdf33cf5aa325fd29a6fd39b4f798e2f
Idrissa <idrisabkar2018@gmail.com>
2023-08-07 00:37:02 +0300

    added some functionnily to add studen page

--- src/assets/js/manageStudents.js
+++ src/assets/js/manageStudents.js
@@ -6,8 +6,14 @@ const modifyBtn = document.querySelector('.manageStudents__btn--modify')
 const deleteBtn = document.querySelector('.manageStudents__btn--delete')
 const attendeesBtn = document.querySelector('.manageStudents__btn--attendees')
 const coursesBtn = document.querySelector('.manageStudents__btn--courses')
-const closeBtn = document.querySelectorAll('.close')
+const closeBtn = document.querySelector('.close')
 
 modifyBtn.addEventListener('click', () => {
-  modifyPopUp.classList.toggle('pop')
+  modifyPopUp.classList.add('shown')
+  modifyPopUp.classList.toggle('hidden')
+})
+
+closeBtn.addEventListener('click', () => {
+  modifyPopUp.classList.remove('shown')
+  modifyPopUp.classList.toggle('hidden')
 })
