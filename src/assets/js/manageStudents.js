706f9264060edd449b99a9b64149d8bc4acceb08
Idrissa <idrisabkar2018@gmail.com>
2023-08-07 13:35:02 +0300

    edit student UI terminer

--- src/assets/js/manageStudents.js
+++ src/assets/js/manageStudents.js
@@ -2,25 +2,35 @@
   if (typeof modifyPopUp !== 'undefined') delete modifyPopUp
 
   var modifyPopUp = document.querySelector('.modifyStudent')
-  var deletePopUp = document.querySelector('.deleteStudent')
-  var attendeesPopUp = document.querySelector('.attendees')
-  var coursesPopUp = document.querySelector('.courses')
-  var modifyBtn = document.querySelectorAll('.manageStudents__btn--modify')
-  var deleteBtn = document.querySelector('.manageStudents__btn--delete')
-  var attendeesBtn = document.querySelector('.manageStudents__btn--attendees')
-  var coursesBtn = document.querySelector('.manageStudents__btn--courses')
-  var closeBtn = document.querySelector('.close')
+  const deletePopUp = document.querySelector('.deleteStudent')
+  const attendeesPopUp = document.querySelector('.attendees')
+  const coursesPopUp = document.querySelector('.courses')
+  const modifyBtn = document.querySelectorAll('.manageStudents__btn--modify')
+  const deleteBtn = document.querySelector('.manageStudents__btn--delete')
+  const attendeesBtn = document.querySelector('.manageStudents__btn--attendees')
+  const coursesBtn = document.querySelector('.manageStudents__btn--courses')
+  const closeBtn = document.querySelector('.close')
+  const form_container = document.querySelector('.editStudent__container')
+
+  //editStudent__container--show
 
   modifyBtn.forEach((btn) => {
     btn.addEventListener('click', () => {
       modifyPopUp.classList.add('shown')
       modifyPopUp.classList.toggle('hidden')
-      console.log('modify')
+      form_container.classList.add('editStudent__container--show')
     })
   })
 
   closeBtn.addEventListener('click', () => {
     modifyPopUp.classList.remove('shown')
     modifyPopUp.classList.toggle('hidden')
+    form_container.classList.remove('editStudent__container--show')
+  })
+  modifyPopUp.addEventListener('click', (e) => {
+    if (e.target === modifyPopUp) {
+      modifyPopUp.classList.remove('shown')
+      modifyPopUp.classList.toggle('hidden')
+    }
   })
 })()
