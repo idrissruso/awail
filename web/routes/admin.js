43da803743230a75052db76ee60eabf675bb46de
Idrissa <idrisabkar2018@gmail.com>
2023-08-19 01:02:40 +0300

    added authentication fun

--- web/routes/admin.js
+++ web/routes/admin.js
@@ -2,6 +2,7 @@ import express from 'express'
 
 const router = express.Router()
 router.get('/', (req, res) => {
+  console.log(req.user)
   res.render('root.ejs', { user: req.user })
 })
 
--- web/routes/home.js
+++ web/routes/home.js
@@ -8,50 +8,29 @@ router.get('/', (req, res) => {
 })
 
 // POST route with Passport.js authentication
-router.post(
-  '/',
-  passport.authenticate('local', {
-    failureRedirect: '/',
-  }),
-  async (req, res, next) => {
-    console.log(req.body) // This function will execute after authentication (if successful)
-    // You can add your POST request handling logic here
-    try {
-      if (req.isAuthenticated()) {
-        // Only execute this block if the user is authenticated
-        // Add your custom POST request handling logic here
-        res.send('POST request handled successfully.')
-      } else {
-        res.send('User is not authenticated.')
-      }
-    } catch (error) {
-      next(error)
+router.post('/', (req, res, next) => {
+  passport.authenticate('local', (err, user, info) => {
+    if (err) {
+      return next(err)
     }
-  }
-)
+    if (!user) {
+      return res.render('index.ejs', { message: info.message })
+    }
+    req.logIn(user, (err) => {
+      if (err) {
+        return next(err)
+      }
+      const role = req.user.role
+      const redirectPath = `/${role}`
+      res.redirect(redirectPath)
+    })
+  })(req, res, next)
+})
 
 router.get('/logout', (req, res) => {
-  req.logout()
-  res.redirect('/')
+  req.logout(() => {
+    res.redirect('/')
+  })
 })
 
 export default router
-
-//const getUser = (username) => {
-//  return axios
-//    .get('http://localhost:3000/api/getUserByUsername/' + username)
-//    .then((response) => {
-//      console.log(response.data)
-//      return response.data
-//    })
-//    .catch((error) => {
-//      if (error.response && error.response.status === 404) {
-//        // Handle case where user was not found
-//        return null
-//      } else {
-//        // Handle any other errors here
-//        console.error(error)
-//      }
-//      return null
-//    })
-//}
