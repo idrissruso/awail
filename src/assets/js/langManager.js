3b96816c170d686eec68b637ac903317a4b491c4
Idrissa <idrisabkar2018@gmail.com>
2023-09-28 19:59:31 +0300

    quelques rerouches teacherProfile

--- src/assets/js/langManager.js
+++ src/assets/js/langManager.js
@@ -9,33 +9,47 @@
     return currentLanguage
   }
 
-  function updateElementTranslation(idKey) {
+  function updateElementTranslation(idKey, op) {
     const language = getCurrentLanguage()
-    fetch(`${language}.json`)
-      .then((response) => response.json())
-      .then((translations) => {
-        const translation = translations[idKey]
-        const element = document.getElementById(idKey)
-        if (element) {
-          element.innerHTML = translation
-        }
-      })
+    if (op) {
+      console.log(op)
+      fetch(`../${language}.json`)
+        .then((response) => response.json())
+        .then((translations) => {
+          const translation = translations[idKey]
+          const element = document.getElementById(idKey)
+          if (element) {
+            element.innerHTML = translation
+          }
+        })
+    } else {
+      console.log(op)
+      fetch(`${language}.json`)
+        .then((response) => response.json())
+        .then((translations) => {
+          const translation = translations[idKey]
+          const element = document.getElementById(idKey)
+          if (element) {
+            element.innerHTML = translation
+          }
+        })
+    }
   }
 
-  const switchLang = (id) => {
+  const switchLang = (id, op) => {
     const languageSelect = document.getElementById('language-select')
     const currentLanguage = languageSelect.value
     setCurrentLanguage(currentLanguage)
-    updateElementTranslation(id)
+    updateElementTranslation(id, op)
     languageSelect.addEventListener('change', (event) => {
       const newLanguage = event.target.value
       setCurrentLanguage(newLanguage)
-      updateElementTranslation(id)
+      updateElementTranslation(id, op)
     })
   }
 
   // In langManager.js
-  window.switchLang = function (id) {
-    switchLang(id)
+  window.switchLang = function (id, op = false) {
+    switchLang(id, op)
   }
 })()
